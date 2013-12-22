a729cae347d3e1debcef700ef2400fd39e70863a
marker-xu <gggfreak@126.com>
2013-12-22 11:15:47 +0800

    登录效果

--- application/views/user/login.php
+++ application/views/user/login.php
@@ -12,15 +12,8 @@
 <%block name="bd"%>
 <div id="bd">
 <h3>How do you see?</h3>
-<div>
-<a href="javascript:void(0);"  class="btn_login" style="float: right;
-width: 76px;
-height: 24px;
-border: 0;
-background: 0;
-filter: none;
-background-image: url(http://pub.idqqimg.com/qconn/wiki/wikiv2//sprite/style.png?max_age=19830212&d=20131218142451);
-background-position: -139px -29px;"></a>您还没有登录，
+<div class="width:166px;">
+<a href="javascript:void(0);"  class="btn btn_login" ></a>您还没有登录，
 </div>
 </div>
 <%/block%>
