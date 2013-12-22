c381c820352a3ff870b967b8732916bdcd30f108
marker-xu <gggfreak@126.com>
2013-12-22 11:07:37 +0800

    login

---
+++ application/views/user/login.php
@@ -0,0 +1,30 @@
+<%extends file="common/base.tpl"%>
+
+<%block name="title"%>登陆<%/block%>
+<%block name="view_conf"%>
+<%/block%>
+
+<%block name="custom_css"%>
+<%/block%>
+
+<%block name="custom_js"%>
+<%/block%>
+<%block name="bd"%>
+<div id="bd">
+<h3>How do you see?</h3>
+<div>
+<a href="javascript:void(0);"  class="btn_login" style="float: right;
+width: 76px;
+height: 24px;
+border: 0;
+background: 0;
+filter: none;
+background-image: url(http://pub.idqqimg.com/qconn/wiki/wikiv2//sprite/style.png?max_age=19830212&d=20131218142451);
+background-position: -139px -29px;"></a>您还没有登录，
+</div>
+</div>
+<%/block%>
+
+<%block name="foot_js"%>
+
+<%/block%>
