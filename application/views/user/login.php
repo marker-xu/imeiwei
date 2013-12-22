9394942a3e1dbe451a7813c150589e602490b453
marker-xu <gggfreak@126.com>
2013-12-22 20:18:44 +0800

    登录框

--- application/views/user/login.php
+++ application/views/user/login.php
@@ -13,7 +13,7 @@
 <div id="bd">
 <h3>How do you see?</h3>
 <div style="width:186px;">
-<a href="javascript:void(0);"  class="btn_login btn " ></a>您还没有登录，
+<a href="javascript:void(0);"  class="btn_login" ></a>您还没有登录，
 </div>
 </div>
 <%/block%>
@@ -23,7 +23,7 @@
 var login_url = "<%$login_url%>";     
 	$(document).ready(function(){
 		$(".btn_login").click(function(){
-			var oauth_login_window = window.open(url, "oauth_login_window", "width=700,height=600,toolbar=yes,menubar=yes,resizable=yes,status=yes");
+			var oauth_login_window = window.open(login_url, "oauth_login_window", "width=700,height=600,toolbar=yes,menubar=yes,resizable=yes,status=yes");
 
 		    oauth_login_window.focus();
 		});
