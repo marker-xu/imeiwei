e800146b973fb6bce7a0c6860e58f1e9ebd2aa82
marker-xu <gggfreak@126.com>
2014-01-15 18:42:23 +0800

    设置

--- application/classes/controller/admin/shop.php
+++ application/classes/controller/admin/shop.php
@@ -1,12 +1,19 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 class Controller_Admin_Shop extends Controller {
+    
+    public function before() {
+        parent::before();
+        $this->template->set("current_action", $this->request->action() );
+    }
 
 	public function action_index()
 	{
 		//$this->request->forward('guide');
 		$this->template->set("abc", "kaka");
-		$this->template()->set_filename("admin/shop/index");
+	}
+	
+	public function action_environment() {
 	}
 
 } // End Welcome
--- application/views/admin/shop/index.php
+++ application/views/admin/shop/index.php
@@ -1,6 +1,6 @@
 <%extends file="common/base.tpl"%>
 
-<%block name="title"%>欢迎来到爱美味<%/block%>
+<%block name="title"%>欢迎来到爱美味管理平台<%/block%>
 <%block name="view_conf"%>
 <%/block%>
 
