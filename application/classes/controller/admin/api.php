e7b2bdf8c1000606420543afd43ba8ec050b1773
marker-xu <gggfreak@126.com>
2014-01-17 19:06:17 +0800

    默认返回全部省份
    ISSUE=IGNORE

--- application/classes/controller/admin/api.php
+++ application/classes/controller/admin/api.php
@@ -3,13 +3,12 @@
 class Controller_Admin_Api extends Controller {
 	
 	public function action_get_region_async() {
-	    $province = $this->request->param("province");
+	    $province = $this->request->param("province", "北京");
 	    $all = $this->request->param("all");
 	    $path = DOCROOT . 'resource/js/third/region/region.json';
 	    $provinces = json_decode(file_get_contents($path), true);
 	    
 	    $result = array();
-	    
 	    // 如果all==1，返回该省的四级以内的地址和其他省的一级地址，否则直接返回该省的信息
 	    if($all == '1') {
 	        foreach($provinces as $key => $item) {
@@ -29,6 +28,11 @@ class Controller_Admin_Api extends Controller {
 	                $result = $item;
 	                break;
 	            }
+	        }
+	    }
+	    if(!$province) {
+	        foreach($provinces as $key => $item) {
+	                $result[] = $item[0];
 	        }
 	    }
 	    $this->ok( $result );
