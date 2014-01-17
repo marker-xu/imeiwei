43fb0b06f0808098d292f3b9af7b89841d9f2ec6
marker-xu <gggfreak@126.com>
2014-01-17 18:42:04 +0800

    增加all参数
    ISSUE=IGNORE

--- application/classes/controller/admin/api.php
+++ application/classes/controller/admin/api.php
@@ -4,6 +4,7 @@ class Controller_Admin_Api extends Controller {
 	
 	public function action_get_region_async() {
 	    $province = $this->request->param("province");
+	    $all = $this->request->param("all");
 	    $path = DOCROOT . 'resource/js/third/region/region.json';
 	    $provinces = json_decode(file_get_contents($path), true);
 	    
