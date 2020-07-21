d358ebae056db1f6c4e13cfaed8c189b42fe0017
xucongbin <xucongbin@qutoutiao.net>
2020-07-21 10:16:21 +0800

    提交

--- application/unittest/demo.php
+++ application/unittest/demo.php
@@ -6,11 +6,20 @@ class DemoTest extends UnitTestCase
     {
         parent::__construct();
     }
-    
+
+    /**
+     * 
+     */
     function setUp() {}
-    
+
+    /**
+     *
+     */
     function tearDown() {}
 
+    /**
+     *
+     */
     function test_test()
     {
         $str = '剧情简介：山姆·维特维奇（希亚·拉博夫 饰）成功阻止了“霸天虎”和“汽车人”两派变形金刚的对决，拯救了全世界的两年后，他虽然被视为英雄人物，但是他还是一个青少...';
